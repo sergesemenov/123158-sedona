c9b8264710b84db7172e06965aa4d1ae81603c66
Sergey <semenoff.serge@gmail.com>
2017-02-19 23:06:17 +0400

    Минифицировал JavaScript

---
+++ js/scripts-min.js
@@ -0,0 +1,5 @@
+var searchOpen=document.querySelector(".js-open-btn"),searchBlock=document.querySelector(".hotel-search-block");searchOpen.addEventListener("click",function(e){e.preventDefault(),searchBlock.classList.toggle("hotel-search-block-show")});
+
+
+
+
